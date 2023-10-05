2dab65fed9f46de0f856cd2d0aec95bb0fa511a8
MrShadowKnight <volodymyr.pinchak@gmail.com>
2023-10-05 18:14:29 +0300

    Add.changes

--- script/index.js
+++ script/index.js
@@ -1,7 +1,11 @@
 function showPassword(selector) {
-  // get element
+  // get element)
   const TARGET = document.querySelector(selector);
-  TARGET.setAttribute("type", "text");
+  const TARGET_VALUE = TARGET.getAttribute("type");
+  TARGET_VALUE == "password"
+    ? TARGET.setAttribute("type", "text")
+    : TARGET.setAttribute("type", "password");
+
   // hide eye icon
 }
 
@@ -11,18 +15,27 @@ const getUserRole = (element) => {
   return DATA;
 };
 
-const getLastNameUser = (element) => {
+const getDataFromField = (element) => {
   const VALUE = element.value;
-  if (VALUE.length >= 3) {
-    return VALUE;
-  } else {
-    alert("Lastname to short");
-    return null;
+  const TYPE = element.getAttribute("name");
+
+  if (TYPE == "last_name") {
+    if (VALUE.length >= 3) {
+      return VALUE;
+    } else {
+      alert("Lastname to short");
+      return null;
+    }
   }
+  // need delete for production ##
+  return VALUE;
 };
 
 // start point
 document.addEventListener("DOMContentLoaded", () => {
+  // read from LS
+  const USER_DATA = JSON.parse(localStorage.getItem("user"));
+  console.log(USER_DATA);
   // get element from document
   const LEFT_BLOCK = document.querySelector(".form__top_LFor_Lblock");
   const RIGHT_BLOCK = document.querySelector(".form__top_LFor_Rblock");
@@ -30,8 +43,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const CHECK_BOX = document.querySelector(".form__input_checkbox");
   const SUBMIT_BTN = document.querySelector(".form__input_sumbit");
   const REGISTER_FORM = document.querySelector(".form");
+  // get user data element
+  const INPUT_NAME = document.querySelector(".form__input_first_name");
+  const INPUT_EMAIL = document.querySelector(".form__input_email");
   const INPUT_LAST_NAME = document.querySelector(".form__input_second_name");
+  const INPUT_PASSWORD = document.querySelector(".form__input_password");
+  const INPUT_CONFIRM_PASSWORD = document.querySelector(
+    ".form__input_password_reat"
+  );
 
+  if (USER_DATA) {
+    const { userName, userLastName, userEmail, userPassword } = USER_DATA;
+    INPUT_NAME.value = userName;
+    INPUT_LAST_NAME.value = userLastName;
+    INPUT_EMAIL.value = userEmail;
+  }
   // switch user role
   LF_BLOCK.addEventListener("click", () => {
     LEFT_BLOCK.classList.toggle("active");
@@ -50,12 +76,35 @@ document.addEventListener("DOMContentLoaded", () => {
     e.preventDefault();
     // get data form
     let role = getUserRole(LF_BLOCK);
-    let lastUserName = getLastNameUser(INPUT_LAST_NAME);
+    let userLastName = getDataFromField(INPUT_LAST_NAME);
+    let userName = getDataFromField(INPUT_NAME);
+    let userEmail = getDataFromField(INPUT_EMAIL);
+    let userPassword = getDataFromField(INPUT_PASSWORD);
+    let userConfirmPassword = getDataFromField(INPUT_CONFIRM_PASSWORD);
+
+    // validation data
+    if (userConfirmPassword != userPassword) {
+      console.log("Password don't match, please try again!");
+      return;
+    }
+
     // create data frame
     const USER_DATA = {
       role,
-      lastUserName,
+      userName,
+      userLastName,
+      userEmail,
+      userPassword,
     };
+
+    // set to LS
+    localStorage.setItem("user", JSON.stringify(USER_DATA));
+    // set to SS
+    sessionStorage.setItem("user", JSON.stringify(USER_DATA));
+    // set to Cookie
+    const cookieObj = JSON.stringify(USER_DATA);
+    document.cookie = `user=${cookieObj}; expires=Thu, 01 Jan 2099 00:00:00 UTC; path="/"`;
+
     console.log("USER_DATA", USER_DATA);
   });
 });
